ea7080d9bd16fc520a3876180c9ec6333c20cd31
imadraude <59265133+imadraude@users.noreply.github.com>
2024-07-12 15:21:58 +0300

    Updates

---
+++ precache-manifest.75370e3a313ec9b0ec57f7a3f09d2274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0073fcadb38d4a90e77320866b460c6d",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "4a8e51ebc260effc7df3",
+    "url": "/about/static/css/main.a5c0f8c2.chunk.css"
+  },
+  {
+    "revision": "a12a6e3152c2f388d171",
+    "url": "/about/static/js/2.edd5718e.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/about/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "4a8e51ebc260effc7df3",
+    "url": "/about/static/js/main.e5db0e5d.chunk.js"
+  },
+  {
+    "revision": "00cdf7c57911bdfba75c",
+    "url": "/about/static/js/runtime~main.dac18c46.js"
+  }
+]);
